549ec9c740b70582cb4ff24096ca808a6d85be30
Bob German <Bob.German@microsoft.com>
2023-10-17 17:16:08 -0400

    Fixed config module

--- src/copilot-me/Lab02-NorthwindProducts/NorthwindProducts/src/config.ts
+++ src/copilot-me/Lab02-NorthwindProducts/NorthwindProducts/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   botId: process.env.BOT_ID,
   botPassword: process.env.BOT_PASSWORD,
-  tableConnectionString: process.env.TABLE_CONNECTION_STRING
+  tableConnectionString: process.env.TABLE_STORAGE_CONNECTION_STRING
 };
 
 export default config;
